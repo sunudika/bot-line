47840285320150372a9621a8483bd16959b47c7b
Sunu Ilham Pradika <sunudika@gmail.com>
2020-04-09 08:48:35 +0700

    add another command message

--- index.php
+++ index.php
@@ -53,6 +53,31 @@ $app->post('/', function ($request, $response)
 			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
 			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
 		}
+		if(strtolower($userMessage) == 'terima kasih')
+		{
+			$message = "sama-sama :)";
+            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+		}
+		if(strtolower($userMessage) == 'ini bot apa?')
+		{
+			$message = "saya adalah bot yang didesain untuk melakukan penjumlahan sederhana, cukup ketikan '[angka1] [angka2]' pada chat selanjutnya dan anda akan melihat hasilnya";
+            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+		}
+		if(strtolower($userMessage))
+		{
+			$splittedMessage = explode(" ",$userMessage);
+			$first = (int) $splittedMessage[0];
+			$second = (int) $splittedMessage[1];
+			$hasil = $first + $second;
+			$message = "Hasilnya: $hasil";
+            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+		}
 	}
 	
 
